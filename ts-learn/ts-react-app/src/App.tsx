738fc33b07193bcf8cfcef2bd22ae177138117c1
yibingxiong <yibingxiong@126.com>
2020-12-21 23:13:14 +0800

    笔记

--- ts-learn/ts-react-app/src/App.tsx
+++ ts-learn/ts-react-app/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Button } from 'antd'
+import FuncComp from './components/demo/FuncComp';
+import ClassComp from './components/demo/ClassComp'
+import LoadingHOC from './components/demo/LoadingHOC'
 
 interface Greeting {
     name: string;
 }
 
 export default function App(props: Greeting) {
-    return <Button>{props.name}</Button>
+    return (
+        <>
+            <Button>{props.name}</Button>
+            <FuncComp name="hello" />
+            <ClassComp name="hello" />
+            <LoadingHOC loading={true} name="loading"/>
+        </>
+    )
 }
---
+++ ts-learn/ts-react-app/src/components/demo/LoadingHOC.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ClassComp from './ClassComp';
+
+interface LoadingHOCProps {
+    loading: boolean;
+}
+
+function LoadingHOC<P>(WrappedComponent: React.ComponentType<P>) {
+    return class extends React.Component<P & LoadingHOCProps> {
+        render() {
+            const {
+                loading,
+                ...props
+            } = this.props;
+            return loading?<p>loding</p>: <WrappedComponent {...props as P}/>
+        }
+    }
+}
+
+export default LoadingHOC(ClassComp);
