e48c1843866169ac9f3738a5e83a4281126f15a2
yibingxiong <yibingxiong@126.com>
2021-01-02 15:48:27 +0800

    redux-learn

--- react-learn/src/App.tsx
+++ react-learn/src/App.tsx
@@ -7,17 +7,23 @@ const TestAdvacedGuides = lazy(() => import('./pages/TestAdvacedGuides'));
 
 const UseTS = lazy(() => import('./pages/UseTs'));
 
+const TestRedux = lazy(() => import('./pages/TestRedux'));
+
+
 function App() {
 
   return (
     <React.Fragment>
       <Router>
         <Link to="/">首页</Link><br/>
-        <Link to="/TestAdvacedGuides">测试高级指引</Link>
+        <Link to="/TestAdvacedGuides">测试高级指引</Link><br/>
+        <Link to="/TestRedux">测试使用redux</Link><br/>
+        <Link to="/UseTS">测试使用TS</Link><br/>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route exact path="/" component={UseTS} />
             <Route path="/TestAdvacedGuides" component={TestAdvacedGuides} />
+            <Route path="/TestRedux" component={TestRedux} />
             <Route path="/UseTS" component={UseTS} />
             <Route component={NoMatch} />
           </Switch>
