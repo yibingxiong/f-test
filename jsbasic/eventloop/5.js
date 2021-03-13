b0a1b60ef6ff1398bdd695dcc7aa0ff2c6a8edd9
yibingxiong <yibingxiong@126.com>
2021-03-13 22:31:56 +0800

    做题啊

---
+++ jsbasic/eventloop/5.js
@@ -0,0 +1,21 @@
+function promise1 () {
+  let p = new Promise((resolve) => {
+    console.log('promise1');
+    resolve('1')
+  })
+  return p;
+}
+function promise2 () {
+  return new Promise((resolve, reject) => {
+    reject('error')
+  })
+}
+promise1()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .finally(() => console.log('finally1'))
+
+promise2()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .finally(() => console.log('finally2'))
